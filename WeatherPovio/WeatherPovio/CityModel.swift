9cb4f1375375194521af801c2f4c5e4d882ef1c2
Ljupco Gorgiev <iOS@Ljupcos-Mac-mini.local>
2016-04-02 20:52:30 +0200

    Added model for City which will be used in our app with all variables and methods needed

---
+++ WeatherPovio/WeatherPovio/CityModel.swift
@@ -0,0 +1,13 @@
+//
+//  CityModel.swift
+//  WeatherPovio
+//
+//  Created by Ljupco Gorgiev on 4/2/16.
+//  Copyright © 2016 Developers. All rights reserved.
+//
+
+import Foundation
+
+class CityModel {
+    
+}
