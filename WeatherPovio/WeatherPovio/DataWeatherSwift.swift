e3f389eb6cea068d6bcb4e73ae656d7c4c51ae83
Ljupco Gorgiev <iOS@Ljupcos-Mac-mini.local>
2016-04-02 21:09:38 +0200

    DataWeatherSwift class added new methods for initialize plist file, added, delete, update new dictionary for local ussage

--- WeatherPovio/WeatherPovio/DataWeatherSwift.swift
+++ WeatherPovio/WeatherPovio/DataWeatherSwift.swift
@@ -7,3 +7,133 @@
 //
 
 import Foundation
+
+struct DataWeatherSwift {
+    
+    enum PlistError: ErrorType {
+        case FileNotWritten
+        case FileDoesNotExist
+    }
+    
+    let name:String
+    
+    var sourcePath:String? {
+        guard let path = NSBundle.mainBundle().pathForResource(name, ofType: "plist") else { return .None }
+        return path
+    }
+    
+    var destPath:String? {
+        guard sourcePath != .None else { return .None }
+        let dir = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+        return (dir as NSString).stringByAppendingPathComponent("\(name).plist")
+    }
+    
+    init?(name:String) {
+        
+        self.name = name
+        let fileManager = NSFileManager.defaultManager()
+        
+        guard let source = sourcePath else { return nil }
+        guard let destination = destPath else { return nil }
+        guard fileManager.fileExistsAtPath(source) else { return nil }
+        
+        if !fileManager.fileExistsAtPath(destination) {
+            do {
+                try fileManager.copyItemAtPath(source, toPath: destination)
+            } catch let error as NSError {
+                print("Unable to copy file. ERROR: \(error.localizedDescription)")
+                return nil
+            }
+        }
+    }
+    
+    
+    func getValuesInPlistFile() -> NSDictionary?{
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            guard let dict = NSDictionary(contentsOfFile: destPath!) else { return .None }
+            return dict
+        } else {
+            return .None
+        }
+    }
+    
+    func getMutablePlistFile() -> NSMutableDictionary?{
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            guard let dict = NSMutableDictionary(contentsOfFile: destPath!) else { return .None }
+            return dict
+        } else {
+            return .None
+        }
+    }
+    
+    func getMutableArrayPlistFile() -> NSMutableArray?{
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            guard let array = NSMutableArray(contentsOfFile: destPath!) else { return .None }
+            return array
+        } else {
+            return .None
+        }
+    }
+    
+    func addValuesToPlistFile(dictionary:NSDictionary) throws {
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            let tmpArray  = self.getMutableArrayPlistFile()!
+            tmpArray.addObject(dictionary)
+            if !tmpArray.writeToFile(destPath!, atomically: false) {
+                print("File not written successfully")
+                throw PlistError.FileNotWritten
+            }
+        } else {
+            throw PlistError.FileDoesNotExist
+        }
+    }
+    
+    func updateValuesToPlistFile(dictionary:NSDictionary) throws {
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            let tmpArray  = self.getMutableArrayPlistFile()!
+            for (index, tmpDictionary) in tmpArray.enumerate() {
+                if tmpDictionary.valueForKey(kCityId)?.integerValue == dictionary.valueForKey(kCityId)?.integerValue {
+                    tmpArray.replaceObjectAtIndex(index, withObject:dictionary)
+                    break
+                }
+            }
+            if !tmpArray.writeToFile(destPath!, atomically: false) {
+                print("File not written successfully")
+                throw PlistError.FileNotWritten
+            }
+        } else {
+            throw PlistError.FileDoesNotExist
+        }
+    }
+    
+    func removeDataFromPlistFile(dictionary:NSDictionary) throws {
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            let tmpArray  = self.getMutableArrayPlistFile()!
+            tmpArray.removeObject(dictionary)
+            if !tmpArray.writeToFile(destPath!, atomically: false) {
+                print("File not written successfully")
+                throw PlistError.FileNotWritten
+            }
+        } else {
+            throw PlistError.FileDoesNotExist
+        }
+    }
+    
+    func addArrayToPlistFile(array:NSMutableArray) throws {
+        let fileManager = NSFileManager.defaultManager()
+        if fileManager.fileExistsAtPath(destPath!) {
+            if !array.writeToFile(destPath!, atomically: false) {
+                print("File not written successfully")
+                throw PlistError.FileNotWritten
+            }
+        } else {
+            throw PlistError.FileDoesNotExist
+        }
+    }
+}
