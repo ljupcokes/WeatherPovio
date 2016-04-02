208358f22f3658cd3b14545e6e8336f91ec8362f
Ljupco Gorgiev <iOS@Ljupcos-Mac-mini.local>
2016-04-02 21:03:21 +0200

    CityModel class modified, added methods and variables , added new constants to GlobalConstants class

--- WeatherPovio/WeatherPovio/CityModel.swift
+++ WeatherPovio/WeatherPovio/CityModel.swift
@@ -9,5 +9,27 @@
 import Foundation
 
 class CityModel {
+    var cityId : Int
+    var cityName : String
+    var cityTemperature : Int
+    var cityHumidity : Int
+    var cityDescription : String
     
+    init(dictionary:[String : String]) {
+        cityId = Int(dictionary[kCityId]!)!
+        self.cityName = dictionary[kCityName]!
+        self.cityTemperature = Int(dictionary[kCityTemperature]!)!
+        self.cityHumidity = Int(dictionary[kCityHumidity]!)!
+        self.cityDescription = dictionary[kCityDescription]!
+    }
+    
+    
+    func updateWithDictionary(dict:Dictionary<String,AnyObject>) {
+        
+    }
+    
+    func returnCityInfoDictionary() -> [String : String] {
+        let cityDictionary : [String: String] = [kCityId : String(cityId), kCityName : cityName, kCityTemperature : String(cityTemperature), kCityHumidity : String(cityHumidity), kCityDescription : cityDescription]
+        return cityDictionary
+    }
 }
--- WeatherPovio/WeatherPovio/GlobalConstants.swift
+++ WeatherPovio/WeatherPovio/GlobalConstants.swift
@@ -9,3 +9,20 @@
 import Foundation
 
 let kPlistKey = "DataWeatherPlist"
+
+let kAppID = "2d900e7d3f723d5232a7fd4fbb40abdc"
+let kAddCity = "http://api.openweathermap.org/data/2.5/weather?q="
+let kListCities = "http://api.openweathermap.org/data/2.5/group?id="
+
+let kKeyCodError = "cod"
+let kMessageError = "message"
+let kKeyMain = "main"
+let kKeyWeather = "weather"
+let kKeyList = "list"
+
+let kCityId = "id"
+let kCityName = "name"
+let kCityHumidity = "humidity"
+let kCityTemperature = "temp"
+let kCityDescription = "description"
+
