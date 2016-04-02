ea74ff483f7cf86454983adbe2c333d2db24b767
Ljupco Gorgiev <iOS@Ljupcos-Mac-mini.local>
2016-04-02 20:56:12 +0200

    Added class which we will use for global constants which we will use for handling some request and response , saving local data and some hardcoded mesages

---
+++ WeatherPovio/WeatherPovio/GlobalConstants.swift
@@ -0,0 +1,11 @@
+//
+//  GlobalConstants.swift
+//  WeatherPovio
+//
+//  Created by Ljupco Gorgiev on 4/2/16.
+//  Copyright © 2016 Developers. All rights reserved.
+//
+
+import Foundation
+
+let kPlistKey = "DataWeatherPlist"
